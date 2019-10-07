f4ab8b22eefd2cdf5d039111fd9b42b536b4ddb7
Vash <vash_dn@mail.ru>
2019-10-07 11:55:53 +0300

    Fix

--- src/App.js
+++ src/App.js
@@ -14,7 +14,19 @@ const App = () => {
     <>
       <YMaps>
         <Map defaultState={mapData} width="100%" height="100vh">
-          {coordinates.map((coordinate, i) => <Placemark key={i} geometry={coordinate} />)}
+          {coordinates.map((coordinate, i) => <Placemark key={i} geometry={coordinate} 
+           options={{
+            // Options. You must specify this type of layout.
+            iconLayout: 'default#image',
+            // Custom image for the placemark icon.
+            iconImageHref: "./img/control.png",
+            // The size of the placemark.
+            iconImageSize: [20, 20],
+            // The offset of the upper left corner of the icon relative
+            // to its "tail" (the anchor point).
+            iconImageOffset: [0, 0],
+          }}
+          />)}
         </Map>
       </YMaps>
     </>
