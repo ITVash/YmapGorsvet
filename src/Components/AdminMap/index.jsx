699bfede1132c8c2e0d77aaa1e6cb31fe055892c
Vash <vash_dn@mail.ru>
2019-10-15 22:16:11 +0300

    Сделал центрирование созданного маркера

--- src/Components/AdminMap/index.jsx
+++ src/Components/AdminMap/index.jsx
@@ -1,17 +1,27 @@
 import React, { useState } from 'react'
-import { YMaps, Map, Placemark, GeoObject } from 'react-yandex-maps';
+import { YMaps, Map, Placemark } from 'react-yandex-maps';
 const AdminMap = props => {
   const [ coordinats, setCoordinats ] = useState({lat:'', lng:''});
   const geocode = ymaps => {
-    ymaps.geocode('Донецк').then(result => {console.log('coords', result.geoObjects.get(0)); setCoordinats({Geo : result.geoObjects.get(0).geometry.getCoordinates() })});
+    ymaps.geocode('Украина, г. Донецк, ул. Лазника, 19')
+      .then(res => {
+        let obj = res.geoObjects.get(0);
+        let coords = obj.geometry.getCoordinates();
+        console.log('Адерс поиска: ', coords );
+      })
   };
   const mapData = {
     center: [48.015875, 37.801341],
     zoom: 12,
   };
+  const mapData2 = {
+    center: [coordinats.lat, coordinats.lng],
+    zoom: 12,
+  };
   return (
     <YMaps onApiAvaliable={ymaps => console.log('Maping', ymaps)} >
-      <Map state={mapData} width="100%" height="100vh" modules={['geocode']}
+      <Map state={coordinats.lat === '' ? mapData : mapData2 } width="100%" height="100vh" modules={['geocode']}
+        onLoad={ymaps => geocode(ymaps)}
         onClick={ e => {
           let coords = e.get('coords');
           setCoordinats({lat: coords[0], lng: coords[1]})
